d04cffc819e43e711446b5878749d20cf5b3edb2
t00l13 <itollik@yandex.ru>
2022-07-17 19:22:07 +0300

    add sort in popupAddNews

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,6 +1,7 @@
 export default class NewsCard {
     constructor({data}, templateSelector) {
         this._templateSelector = templateSelector;
+        this._category = data.category;
         this._title = data.title;
         this._description = data.description;
     }
@@ -20,9 +21,13 @@ export default class NewsCard {
 
         this._newsTitle = this._news.querySelector('.news__title');
         this._newsDescription = this._news.querySelector('.news__subtitle');
+        this._newsCategory = this._news.dataset;
+        this._newsHead = this._news.querySelector('.news__head');
 
         this._newsTitle.textContent = this._title;
         this._newsDescription.textContent = this._description;
+        this._newsCategory.textContent = this._category;
+        this._newsHead.textContent = this._category;
 
         return this._news;
     }
